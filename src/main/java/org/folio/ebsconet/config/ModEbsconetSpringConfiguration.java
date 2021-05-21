ad6bc0e15a345acea799f6d8e03a9c11c98738c6
Yauheni Kuzmianok <yauheni_kuzmianok@epam.com>
2021-05-21 18:16:58 +0300

    MODEBSNET-9 rancher pipeline
    Signed-off-by: Yauheni Kuzmianok <yauheni_kuzmianok@epam.com>

--- src/main/java/org/folio/ebsconet/config/ModEbsconetSpringConfiguration.java
+++ src/main/java/org/folio/ebsconet/config/ModEbsconetSpringConfiguration.java
@@ -4,11 +4,23 @@ import com.fasterxml.jackson.databind.Module;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class ModEbsconetSpringConfiguration {
+
   @Bean
   public Module jsonNullableModule() {
     return new JsonNullableModule();
+  }
+
+  @Bean
+  public CommonsRequestLoggingFilter requestLoggingFilter() {
+    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+    loggingFilter.setIncludeClientInfo(true);
+    loggingFilter.setIncludeQueryString(true);
+    loggingFilter.setIncludePayload(true);
+    loggingFilter.setMaxPayloadLength(64000);
+    return loggingFilter;
   }
 }
